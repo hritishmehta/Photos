b5b7fb0bbfe993419e2c97936f53a0463c266636
Likhit Krishnam <likhit.krishnam2003@gmail.com>
2023-04-26 19:42:42 -0400

    xml files

--- app/src/main/java/com/example/photosandroid/MainActivity.java
+++ app/src/main/java/com/example/photosandroid/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.photosandroid;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button searchButton;
+    private Button openButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_page);
+        searchButton=findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.photo_page);
+            }
+        });
     }
+
 }
+
---
+++ app/src/main/java/com/example/photosandroid/slideshowController.java
@@ -0,0 +1,4 @@
+package com.example.photosandroid;
+
+public class slideshowController {
+}
