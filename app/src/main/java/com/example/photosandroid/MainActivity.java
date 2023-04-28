3927d79b6de37344e26a486423a82fc15ef30f93
hritishmehta <82344097+hritishmehta@users.noreply.github.com>
2023-04-27 22:09:08 -0400

    test

--- app/src/main/java/com/example/photosandroid/MainActivity.java
+++ app/src/main/java/com/example/photosandroid/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.photosandroid;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-
+import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -49,6 +51,39 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<Album>(this,R.layout.album_page,user.getAllAlbums());
         albumListView.setAdapter(arrayAdapter);
 
+        albumListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+                Album selectedAlbum = (Album)albumListView.getSelectedItem();
+                return true;
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                final EditText albumNameInput = new EditText(MainActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                // Set the title and message of the dialog
+                builder.setTitle("Create New Album");
+                builder.setMessage("Enter the name of the new album:");
+
+                // Set the view of the dialog to be the EditText input view
+                builder.setView(albumNameInput);
+
+                builder.setPositiveButton("Create",new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int which){
+                        String albumName = albumNameInput.getText().toString();
+                        Album addition = new Album(albumName);
+                        MainActivity.user.addAlbum(addition);
+                        arrayAdapter.add(addition);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                });
+
+            }
+        });
     }
 
 }
